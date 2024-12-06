06788403925476fb67a4d03101393cf44256a302
Harshit <raizadaharshit2004@gmail.com>
2024-12-06 21:58:04 +0530

    program schema update-2

---
+++ prisma/migrations/20241206162732_program_schema_update_2/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "Program" ADD COLUMN     "canFinishIn" TEXT,
+ADD COLUMN     "creditsInIITIIM" TEXT,
+ADD COLUMN     "creditsInUS" TEXT,
+ADD COLUMN     "gloveraPricing" TEXT,
+ADD COLUMN     "iitIim" TEXT,
+ADD COLUMN     "originalPricing" TEXT,
+ADD COLUMN     "savings" TEXT,
+ADD COLUMN     "savingsPercentage" TEXT,
+ADD COLUMN     "totalCredits" TEXT;
